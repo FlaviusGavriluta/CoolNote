3cc7e4a956ef450d7aea30a601bc493938b23538
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-10-20 19:16:28 +0300

    new project

---
+++ src/test/java/com/example/coolnotejavaflaviusgavriluta/CoolNoteJavaFlaviusGavrilutaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coolnotejavaflaviusgavriluta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolNoteJavaFlaviusGavrilutaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
