1020cb7b9ea479c9c9097728e7fde9e4ac89b7dc
flaviusgavriluta <flavius.gavriluta@gmail.com>
2023-12-08 14:41:35 +0200

    finished third task

--- src/main/java/com/example/coolnote/NoteController.java
+++ src/main/java/com/example/coolnote/NoteController.java
@@ -1,5 +1,6 @@
 package com.example.coolnote;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +10,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/")
 public class NoteController {
+    static String note;
+    static int editCount = 0;
+
     @GetMapping(value = "note")
     public String displayAddNoteForm() {
         return "note";
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAtribute("note", note);
+        model.addAtribute("editCount", editCount);
+        return "index";
     }
 
     @PostMapping(value = "note")
