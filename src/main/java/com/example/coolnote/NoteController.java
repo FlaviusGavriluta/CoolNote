8c8c9e8f73c2b9d0a5822d7a15cd06ebdf6978bc
flaviusgavriluta <flavius.gavriluta@gmail.com>
2023-12-08 08:51:43 +0200

    second task finished

--- src/main/java/com/example/coolnote/NoteController.java
+++ src/main/java/com/example/coolnote/NoteController.java
@@ -1,4 +1,22 @@
 package com.example.coolnote;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
 public class NoteController {
+    @GetMapping(value = "note")
+    public String displayAddNoteForm() {
+        return "note";
+    }
+
+    @PostMapping(value = "note")
+    public String processAddNoteForm(@RequestParam String newNote) {
+        // Redirect to "/"
+        return "redirect:";
+    }
 }
